352aef858ca3d3aa9df7fcba3d705b9be7a9610e
Sirisha <63629781+Sirisha1706@users.noreply.github.com>
2025-07-02 23:30:53 +0530

    Delete backend directory

--- backend/routes/petRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPetInfo } = require("../controllers/petInfoController");
-const { restrictToLoggedInUserOnly } = require("../middlewares/auth");
-
-// GET /pets/:category?size=&foodType=&search=
-router.get("/:category", getPetInfo);
-
-module.exports = router;
--- backend/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const {handleUserSignup,handleUserLogin}=require('../controllers/userController.js');
-
-const userRouter=express.Router();
-
-userRouter.post('/signup',handleUserSignup);
-userRouter.post('/login',handleUserLogin);
-
-module.exports=userRouter;
