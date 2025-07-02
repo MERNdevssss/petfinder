1e9dd29b3f10a3010e70eb90571c45c7d7715276
khushboo8252 <khushbooranjan8252@gmail.com>
2025-07-02 21:19:33 +0530

    add petSuggestion

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToMongo = require("./dbConnection");
 const cors = require("cors");
+const  petRoutes = require ('./routes/suggestionRoutes.js');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+app.use('/api', petRoutes);
 
 connectToMongo();
 
