5ec2b2a881572d77964077c16eaa0e8124727ee9
Sirisha <63629781+Sirisha1706@users.noreply.github.com>
2025-07-03 08:56:16 +0530

    add missing files

---
+++ backend/dbConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+
+const connectToMongo = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "PetFinder",
+    })
+    .then(() => {
+      console.log(`connected`);
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
+};
+
+module.exports = connectToMongo;
